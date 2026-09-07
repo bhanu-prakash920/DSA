b8830a8ac289598e042b8e9537f33fde2d9127bd
Bhanu Prakash <kushabhanuprakash07@gmail.com>
2026-09-07 22:37:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (75.04%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -14,24 +14,23 @@ public:
 
     int widthOfBinaryTree(TreeNode* root) {
         if(!root) return 0;
-        queue<pair<long long,TreeNode*>> q;
+        queue<pair<int,TreeNode*>> q;
         int maxWidth = 0;
         q.push({0,root});
         while(!q.empty()){
             int n = q.size();
             int width = 1;
-            int mmin = 0;
+            int left,right;
             for(int i = 0; i< n ; i++){
                 TreeNode* curr = q.front().second;
-                if(i == 0) mmin = q.front().first;
-                long long index = q.front().first - mmin;
+                long long  index = q.front().first;
                 q.pop();
-                if(curr->left) q.push({2*index + 1, curr->left});
-                if(curr->right) q.push({2*index + 2, curr->right});
-                if(i == n-1) width += index;
-                if(i == 0) width -= index;
+                if(curr->left) q.push({index*2 + 1, curr->left});
+                if(curr->right) q.push({index*2 + 2, curr->right});
+                if(i == n-1) right = index;
+                if(i == 0) left = index;
             }
-            maxWidth = max(maxWidth , width);
+            maxWidth = max(maxWidth , right - left + 1);
 
 
         }
