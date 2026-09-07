3ac34e784e97a6516bfda435b332c25953007581
Bhanu Prakash <kushabhanuprakash07@gmail.com>
2026-09-07 14:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (77.97%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        queue<TreeNode*> q;
+        if (root)
+            q.push(root);
+        vector<vector<int>> levelOrder;
+        int order = 0;
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> level(n);
+            for (int i = 0; i < n; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+                int j = order? n-1-i : i;
+                level[j] = curr->val;
+            }
+            order = !order;
+            levelOrder.push_back(level);
+        }
+        return levelOrder;
+    }
+};
