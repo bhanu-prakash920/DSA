5e61853a6295ff175cb2942149eb596174c771d0
Bhanu Prakash <kushabhanuprakash07@gmail.com>
2026-09-11 01:15:31 +0530

    Time: 5 ms (72.31%) | Memory: 15.8 MB (64.58%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -6,33 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int count(TreeNode* root){
-        if(!root) return 0;
-        int leftsum = count(root->left);
-        int rightsum = count(root->right);
+    pair<int, int> sum(TreeNode* root, int& ans) {
+        if(!root) return {0,0};
+        auto [leftSum, leftCount] = sum(root->left, ans);
+        auto [rightSum, rightCount] = sum(root->right, ans);
 
-        return leftsum+rightsum+1;
-    }
-    int sum(TreeNode* root, int &ans){
-        if(!root) return 0;
-        int leftsum = sum(root->left , ans);
-        int rightsum = sum(root->right, ans);
-        if((leftsum+rightsum+root->val )/ count(root) == root->val){
+        int sum = leftSum + rightSum + root->val;
+        int count = leftCount + rightCount + 1;
+
+        if (sum / count == root->val) {
             ans++;
         }
 
-        return leftsum+rightsum+root->val;
+        return {sum, count};
     }
     int averageOfSubtree(TreeNode* root) {
         int ans = 0;
-        sum(root,ans);
-        
+        sum(root, ans);
+
         return ans;
-        
     }
 };
